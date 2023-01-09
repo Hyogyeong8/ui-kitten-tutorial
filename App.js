21b589c62f5247b3f8ce640d5337f44fda30a467
Hyogyeong8 <khg4773@gmail.com>
2023-01-09 19:16:53 +0900

    Add: theme.json

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import * as eva from "@eva-design/eva";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+import * as eva from '@eva-design/eva';
 import {
   ApplicationProvider,
   Layout,
   Text,
   Button,
-} from "@ui-kitten/components";
+} from '@ui-kitten/components';
+import { default as theme } from './theme.json';
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
+    <ApplicationProvider
+      {...eva}
+      theme={{ ...eva.light, ...theme }}
+    >
       <Layout style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <StatusBar style='auto' />
       </Layout>
-      <Button title="MyButton" onPress={() => alert("It works")} />
+      <Button onPress={() => alert('It works')}>MyButton</Button>
     </ApplicationProvider>
   );
 }
@@ -23,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
